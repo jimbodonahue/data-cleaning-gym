66c968d9f54ae2ec162851a5854a2f8e5796b7b8
James Donahue <jimbodonahue@gmail.com>
2026-04-29 12:10:11 +0200

    Reverted import command
    Still trying to figure out how to get it to work when hosted. #itWorkedOnMyComputer

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scikit-learn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 import io
 
 import matplotlib.pyplot as plt
